9d9d443e1ede3a753012cb5d27b9dc0c702100f1
Picker <picker@gmail.com>
2019-01-09 14:09:53 -0500

    First working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,61 @@
 $(document).ready(function() {
 
-// Hide the video player and its background
+// Hide the video player and its background on page load
 $("#video-player").hide();
 $("#video-background").hide();
 
-// VARIABLES
+// GLOBAL VARIABLES
 // ==================================================
 
-// List of favorite movies
-var favoriteMovies = [];
+// Hold the keys for APIs and database
+var keys = {
+    OMDb: "trilogy",
+    TMDb: "ee00799d9647027160a387c172fcee51",
+    firebase: "AIzaSyCN1PSw8wmS16qrwUA66gSDcBl0cQuv7JE"
+};
+
+// Define global variable for the user
+var currentUser;
 
 
-// FUNCTIONS FOR LATER USE
+// USER AUTHENTICATION AND DATABASE
 // ==================================================
 
-// Create the list of favorite movies on the web page
-var showFavoriteMovies = function() {
+// Firebase configuration    
+var config = {
+    apiKey: keys.firebase,
+    authDomain: "movie-wish-list-v2.firebaseapp.com",
+    databaseURL: "https://movie-wish-list-v2.firebaseio.com",
+    projectId: "movie-wish-list-v2",
+    storageBucket: "movie-wish-list-v2.appspot.com",
+    messagingSenderId: "378231234702"
+};
 
-        $("#wishlist-container").html("");
+// Firebase initialization
+firebase.initializeApp(config);
 
-        for (i = 0; i < favoriteMovies.length; i++) {
-                  
-            $("#wishlist-container").append("<div class='wishlist'><img class='movie-wish' src='" + favoriteMovies[i] + "'><button class='checkbox' id='" + i + "'>✓</button>");
-    
-        }
+// Define global variable for the database
+var database = firebase.database();
 
-}
+// Trigger anonymous authentication
+firebase.auth().signInAnonymously().catch(function(error) {
+console.log("Authentication error code: " + error.code + ". Message: " + error.message);
+});
+
+// Retrieve the user ID assigned by Firebase
+firebase.auth().onAuthStateChanged(function(user) {
+
+    currentUser = user.uid;
+    console.log("User authentication success. User ID: " + currentUser);
+
+    // Get real-time updates from the database for the current user, and populate the Wish List accordingly
+    database.ref(currentUser).on("child_added", function(snapshot) {
+        var sv = snapshot.val();
+        console.log(sv);           
+        $("#wishlist-container").prepend("<div class='wishlist animated fadeIn'><a href='#'><img class='movie-wish' src='" + sv.poster + "' alt='" + sv.title + "' data-IMDb='" + sv.IMDb + "'></a><button class='checkbox' data-db-id='" + snapshot.key + "'>✓</button>"); 
+    });
+
+});
 
 
 // USER-TRIGGERED BEHAVIORS
@@ -39,7 +69,7 @@ $("#search-movie").on("click", function(event) {
     var movie = $("#movie-input").val().trim();
 
     // Use the OMDb API to get results
-    var OMDBqueryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy";
+    var OMDBqueryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=" + keys.OMDb;
 
     $.ajax({
         url: OMDBqueryURL,
@@ -63,7 +93,7 @@ $("#search-movie").on("click", function(event) {
         for (i = 0; i < maxResults; i++) {
 
             // Populate the web page with results
-            $("#results-div").append("<div class='results animated fadeIn'><img class='result-img' src='" + response.Search[i].Poster + "'><div class='movie-info'><h2>" + response.Search[i].Title + "</h2><h3>Year: " + response.Search[i].Year + "</h3><a href='#' class='watch-trailer' id='" + response.Search[i].imdbID + "' data-title='" + response.Search[i].Title + "'>Watch Trailer</a><a href='https://www.imdb.com/title/" + response.Search[i].imdbID + "' target='_blank'>Open on IMDb</a><button type='button' class='add-button' data-poster='" + response.Search[i].Poster + "'>Add to Wish List</button></div></div>");
+            $("#results-div").append("<div class='results animated fadeInLeft'><img class='result-img' src='" + response.Search[i].Poster + "'><div class='movie-info'><h2>" + response.Search[i].Title + "</h2><h3>Year: " + response.Search[i].Year + "</h3><a href='#' class='watch-trailer' id='" + response.Search[i].imdbID + "' data-title='" + response.Search[i].Title + "'>Watch Trailer</a><a href='https://www.imdb.com/title/" + response.Search[i].imdbID + "' target='_blank'>Open on IMDb</a><button type='button' class='add-button' data-title='" + response.Search[i].Title + "' data-poster='" + response.Search[i].Poster + "' data-IMDb='" + response.Search[i].imdbID + "'>Add to Wish List</button></div></div>");
 
         }
 
@@ -79,7 +109,7 @@ $(document).on("click", ".watch-trailer", function(){
     var clickedMovieTitle = $(this).attr("data-title");
 
     // Use the MovieDB API to add trailers to each result
-    var MDBqueryURL = "https://api.themoviedb.org/3/movie/" + clickedID + "/videos?api_key=ee00799d9647027160a387c172fcee51";
+    var MDBqueryURL = "https://api.themoviedb.org/3/movie/" + clickedID + "/videos?api_key=" + keys.TMDb;
 
     // Return error message if the movie has no trailer available
     function trailerError() {
@@ -98,32 +128,24 @@ $(document).on("click", ".watch-trailer", function(){
     .done(function(trailerResponse) {
 
         if (trailerResponse.results.length > 0) {
-
             $("#video-background").show();
             $("#video-player").show();
             $("#video-player").html("<iframe width='853' height='480' src='https://www.youtube.com/embed/" + trailerResponse.results[0].key + "' frameborder='0' allowfullscreen></iframe>'");
             $("#video-player").append("<button id='close-button' class='close-trailer'>✘</button>");
-        
         }
 
         else {
-
             trailerError();
-
         }
         
     })
 
     .fail(function() {
-
         trailerError();
-
     })
 
     .always(function(trailerResponse) {
-
         console.log(trailerResponse);
-
     });
 
 });
@@ -137,18 +159,15 @@ $(document).on("click", ".close-trailer", function(){
 
 });
 
-
 // Define behavior when an "add" button is clicked
 $(document).on("click", ".add-button", function(){
 
-    // Push new movie ID into the array
-    favoriteMovies.unshift($(this).attr("data-poster"));
-
-    // Save the updated array into local storage
-    localStorage.setItem("favorite-movies", JSON.stringify(favoriteMovies));
-
-    // Show the updated array on the web page
-    showFavoriteMovies();
+    // Push movie details to the database
+    database.ref(currentUser).push({
+        title: $(this).attr("data-title"),
+        poster: $(this).attr("data-poster"),
+        IMDb: $(this).attr("data-IMDb")
+  });
 
 });
 
@@ -156,29 +175,44 @@ $(document).on("click", ".add-button", function(){
 $(document).on("click", ".checkbox", function() {
 
     // Delete the item for which the checkbox was clicked
-    favoriteMovies.splice($(this).attr("id"), 1);
+    var id = $(this).attr("data-db-id");
+    console.log("Deleting: ", id);
+    database.ref(currentUser + "/" + id).remove();
 
-    //Show updated movie list
-    showFavoriteMovies();
+    // Get the updated movie list from the database and show it on the page
+    database.ref(currentUser).once('value', function(snapshot) {
+        $("#wishlist-container").empty();
+        snapshot.forEach(function(childSnapshot) {
+          var childKey = childSnapshot.key;
+          var csv = childSnapshot.val();
+         $("#wishlist-container").prepend("<div class='wishlist animated fadeIn'><a href='#'><img class='movie-wish' src='" + csv.poster + "' alt='" + csv.title + "' data-IMDb='" + csv.IMDb + "'></a><button class='checkbox' data-db-id='" + childKey + "'>✓</button>");
+        });
+    });
 
-    // Save the updated array into local storage
-    localStorage.setItem("favorite-movies", JSON.stringify(favoriteMovies));
+});
 
-  });
+// Define behavior when a wishlist movie poster is clicked
+$(document).on("click", ".movie-wish", function() {
 
+    var clickedMovieId = $(this).attr("data-IMDb");
+    console.log("Clicked Movie ID: " + clickedMovieId);
 
-// BEHAVIOR ON PAGE LOAD
-// ==================================================
+    // Add on-click animation
+    $(this).addClass("animated pulse");
 
-// Populate the array from local storage
-favoriteMovies = JSON.parse(localStorage.getItem("favorite-movies"));
+    // Use the OMDb API to get results
+    var OMDBqueryURL = "https://www.omdbapi.com/?i=" + clickedMovieId + "&apikey=" + keys.OMDb;
 
-// Keep the array empty if there are no movie IDs saved in local storage
-if (!Array.isArray(favoriteMovies)) {
-    favoriteMovies = [];
-  }
+    $.ajax({
+        url: OMDBqueryURL,
+        method: "GET"
+        }).then(function(response) {
 
-// Populate the list of favorite movies on the page
-showFavoriteMovies();
+        // Populate the web page with details on the clicked movie
+        $("#results-div").html("<div class='results animated bounceInRight'><img class='result-img' src='" + response.Poster + "'><div class='movie-info'><h2>" + response.Title + "</h2><h3>Year: " + response.Year + "</h3><a href='#' class='watch-trailer' id='" + response.imdbID + "' data-title='" + response.Title + "'>Watch Trailer</a><a href='https://www.imdb.com/title/" + response.imdbID + "' target='_blank'>Open on IMDb</a></div></div>");
+
+    });
+
+});
 
 });
