289d9455093c4736f045a245b07171d9b993b8e0
Picker <picker@gmail.com>
2019-03-18 18:10:05 -0400

    Fetch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -90,7 +90,7 @@ $("#search-movie").on("click", function(event) {
         fetch("/.netlify/functions/omdb", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({movie: movie})
+                body: JSON.stringify({"movie": movie})
         })
         .then(function(response) {
     
